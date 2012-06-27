534b3e3077e9949b6019c1d49cb571191cb0c023
Nick Ohrn <nohrn@ohrnventures.com>
2012-06-27 11:54:41 -0400

    Settings page now looks awesom.

--- views/backend/settings/settings.php
+++ views/backend/settings/settings.php
@@ -31,7 +31,7 @@
 	<h3><?php _e('This Site'); ?></h3>
 
 	<div class="urtak-field">
-		<p><?php _e('Lorem ipsum dolor sit amet, consectutor.'); ?></p>
+		<p class="urtak-help urtak-help-nomargin"><label for="urtak-credentials-publication-key"><?php _e('Select this site from the list or create a new one.'); ?></label></p>
 		<select name="urtak[credentials][publication-key]" id="urtak-credentials-publication-key">
 			<?php foreach($publications as $publication) { ?>
 			<option data-domains="<?php esc_attr_e(implode(', ', (isset($publication['domains']['url']) ? (array)$publication['domains']['url'] : array()))); ?>" <?php selected($settings['credentials']['publication-key'], $publication['key']); ?> value="<?php esc_attr_e($publication['key']); ?>"><?php esc_html_e($publication['name']); ?></option>
@@ -40,14 +40,14 @@
 		</select>
 
 		<div id="urtak-new-site-dependencies">
-			<p><?php _e('What do you want to name this site?'); ?></p>
-			<input type="text" class="large-text" name="urtak[publication][name]" value="<?php _e(''); ?>" placeholder="<?php _e('Site Name'); ?>" />
+			<p class="urtak-help urtak-help-nomargin"><label for="urtak-publication-name"><?php _e('What do you want to name this site?'); ?></label></p>
+			<input type="text" class="text large-text" name="urtak[publication][name]" value="<?php _e(''); ?>" placeholder="<?php _e('Site Name'); ?>" />
 		</div>
 
-		<p><?php _e('Domains this Urtak Site will run on.'); ?></p>
-		<input type="text" class="large-text code" name="urtak[publication][domains]" id="urtak-publication-domains" value="<?php ?>" />
+		<p class="urtak-help urtak-help-nomargin"><label for="urtak-publication-domains"><?php _e('Domains this Urtak Site will run on.'); ?></label></p>
+		<input type="text" class="text large-text code" name="urtak[publication][domains]" id="urtak-publication-domains" value="<?php ?>" />
 
-		<p id="urtak-publication-key-display-container"><?php _e('Site Key: <span id="urtak-publication-key-display"></span>'); ?></p>
+		<p class="urtak-help urtak-help-nomargin" id="urtak-publication-key-display-container"><?php _e('Site Key: <span id="urtak-publication-key-display"></span>'); ?></p>
 	</div>
 	<input type="hidden" name="urtak[publication][publication-data]" value="<?php esc_attr_e(json_encode($publications)); ?>" />
 
@@ -57,77 +57,90 @@
 
 	<h3 id="urtak-settings-subheader"><?php _e('Placement'); ?></h3>
 
-	<div class="urtak-field">
-		<label class="urtak-checkbox">
-			<input <?php checked($settings['placement'], 'append'); ?> type="radio" name="urtak[placement]" id="urtak-placement-append" value="append" />
-			<?php _e('I want Urtaks automatically appended to my posts'); ?>
-		</label>
-		<p class="urtak-help">
-			<label for="urtak-placement-append">
-				<?php _e('Select this option if you want Urtaks to automatically be added to the end of all your posts.'); ?>
+	<div class="urtak-field" style="padding-bottom: 5px;">
+		<div class="urtak-checkbox-container">
+			<label class="urtak-checkbox">
+				<input <?php checked($settings['placement'], 'append'); ?> type="radio" name="urtak[placement]" id="urtak-placement-append" value="append" />
+				<?php _e('I want Urtaks automatically appended to my posts'); ?>
 			</label>
-		</p>
+			<p class="urtak-help">
+				<label for="urtak-placement-append">
+					<?php _e('Select this option if you want Urtaks to automatically be added to the end of all your posts.'); ?>
+				</label>
+			</p>
+		</div>
 
-		<label class="urtak-checkbox">
-			<input <?php checked($settings['placement'], 'manual'); ?> type="radio" name="urtak[placement]" id="urtak-placement-manual" value="manual" />
-			<?php _e('I will insert Urtaks manually'); ?>
-		</label>
-		<p class="urtak-help">
-			<label for="urtak-placement-manual">
-				<?php _e('Select this option if you want to place your Urtaks manually by using the template tag. Simply copy the following into your theme.'); ?>
+		<div class="urtak-checkbox-container">
+			<label class="urtak-checkbox">
+				<input <?php checked($settings['placement'], 'manual'); ?> type="radio" name="urtak[placement]" id="urtak-placement-manual" value="manual" />
+				<?php _e('I will insert Urtaks manually'); ?>
 			</label>
-		</p>
-		<p class="urtak-help">
-			<input readonly="readonly" type="text" class="code large-text" id="urtak-placement-manual-tag" value="<?php esc_attr_e("<?php do_action('make_urtak_widget'); ?>"); ?>" />
-		</p>
+			<p class="urtak-help">
+				<label for="urtak-placement-manual">
+					<?php _e('Select this option if you want to place your Urtaks manually by using the template tag. Simply copy the following into your theme.'); ?>
+				</label>
+			</p>
+			<p class="urtak-help">
+				<input readonly="readonly" type="text" class="code text large-text" id="urtak-placement-manual-tag" value="<?php esc_attr_e("<?php do_action('make_urtak_widget'); ?>"); ?>" />
+			</p>
+		</div>
+		<div class="clear"></div>
 	</div>
 
 	<div class="urtak-field">
-		<input type="hidden" name="urtak[homepage]" id="urtak-homepage-hidden" value="no" />
-		<label class="urtak-checkbox">
-			<input <?php checked($settings['homepage'], 'yes'); ?> type="checkbox" name="urtak[homepage]" id="urtak-homepage" value="yes" />
-			<?php _e('Include on Homepage'); ?>
-		</label>
-		<p class="urtak-help">
-			<label for="urtak-homepage">
-				<?php _e('If this is unchecked, Urtaks are only displayed on posts and pages.'); ?>
+		<div class="urtak-checkbox-container">
+			<input type="hidden" name="urtak[homepage]" id="urtak-homepage-hidden" value="no" />
+			<label class="urtak-checkbox">
+				<input <?php checked($settings['homepage'], 'yes'); ?> type="checkbox" name="urtak[homepage]" id="urtak-homepage" value="yes" />
+				<?php _e('Include on Homepage'); ?>
 			</label>
-		</p>
+			<p class="urtak-help">
+				<label for="urtak-homepage">
+					<?php _e('If this is unchecked, Urtaks are only displayed on posts and pages.'); ?>
+				</label>
+			</p>
+		</div>
 
-		<input type="hidden" name="urtak[user-start]" id="urtak-user-start-hidden" value="no" />
-		<label class="urtak-checkbox">
-			<input <?php checked($settings['user-start'], 'yes'); ?> type="checkbox" name="urtak[user-start]" id="urtak-user-start" value="yes" />
-			<?php _e('Let Users Start Urtaks'); ?>
-		</label>
-		<p class="urtak-help">
-			<label for="urtak-user-start">
-				<?php _e('Display Urtaks even if they don\'t have any questions and let users start the conversation.'); ?>
+		<div class="urtak-checkbox-container">
+			<input type="hidden" name="urtak[user-start]" id="urtak-user-start-hidden" value="no" />
+			<label class="urtak-checkbox">
+				<input <?php checked($settings['user-start'], 'yes'); ?> type="checkbox" name="urtak[user-start]" id="urtak-user-start" value="yes" />
+				<?php _e('Let Users Start Urtaks'); ?>
 			</label>
-		</p>
+			<p class="urtak-help">
+				<label for="urtak-user-start">
+					<?php _e('Display Urtaks even if they don\'t have any questions and let users start the conversation.'); ?>
+				</label>
+			</p>
+		</div>
 	</div>
 
 	<h3><?php _e('Moderation'); ?></h3>
 	
 	<div class="urtak-field">
-		<label class="urtak-checkbox">
-			<input <?php checked($settings['moderation'], 'community'); ?> type="radio" name="urtak[moderation]" id="urtak-moderation-community" value="community" />
-			<?php _e('Community Moderation'); ?>
-		</label>
-		<p class="urtak-help">
-			<label for="urtak-moderation-community">
-				<?php _e('Some text goes here about what community moderation means, exactly.'); ?>
+		<div class="urtak-checkbox-container">
+			<label class="urtak-checkbox">
+				<input <?php checked($settings['moderation'], 'community'); ?> type="radio" name="urtak[moderation]" id="urtak-moderation-community" value="community" />
+				<?php _e('Community Moderation'); ?>
 			</label>
-		</p>
+			<p class="urtak-help">
+				<label for="urtak-moderation-community">
+					<?php _e('Some text goes here about what community moderation means, exactly.'); ?>
+				</label>
+			</p>
+		</div>
 
-		<label class="urtak-checkbox">
-			<input <?php checked($settings['moderation'], 'publisher'); ?> type="radio" name="urtak[moderation]" id="urtak-moderation-publisher" value="publisher" />
-			<?php _e('Publisher Moderation'); ?>
-		</label>
-		<p class="urtak-help">
-			<label for="urtak-moderation-publisher">
-				<?php _e('Some text goes here about what publisher moderation means, exactly.'); ?>
+		<div class="urtak-checkbox-container">
+			<label class="urtak-checkbox">
+				<input <?php checked($settings['moderation'], 'publisher'); ?> type="radio" name="urtak[moderation]" id="urtak-moderation-publisher" value="publisher" />
+				<?php _e('Publisher Moderation'); ?>
 			</label>
-		</p>
+			<p class="urtak-help">
+				<label for="urtak-moderation-publisher">
+					<?php _e('Some text goes here about what publisher moderation means, exactly.'); ?>
+				</label>
+			</p>
+		</div>
 	</div>
 
 	<h3><?php _e('Language'); ?></h3>
